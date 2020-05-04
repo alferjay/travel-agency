7e7859491c6e97d28b4662bf560b3e497e77ee77
Alfer Jay Salvador <alferjaysalvador@gmail.com>
2020-05-04 13:46:44 +0400

    Updates

---
+++ precache-manifest.dd578374c28bdd6ca7125bb17a133bef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df17e8da1e00b78fbd908f999ca98ec1",
+    "url": "/travel-agency/index.html"
+  },
+  {
+    "revision": "c5c733d19dd9d717247b",
+    "url": "/travel-agency/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "6cb449ef8b15f239fa74",
+    "url": "/travel-agency/static/css/main.a459d713.chunk.css"
+  },
+  {
+    "revision": "c5c733d19dd9d717247b",
+    "url": "/travel-agency/static/js/2.51c60fb6.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/travel-agency/static/js/2.51c60fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb449ef8b15f239fa74",
+    "url": "/travel-agency/static/js/main.5dd45c52.chunk.js"
+  },
+  {
+    "revision": "465ef5f80ebe5b0454b1",
+    "url": "/travel-agency/static/js/runtime-main.95cc4171.js"
+  },
+  {
+    "revision": "c72436372663c14e21a7264767596689",
+    "url": "/travel-agency/static/media/img1.c7243637.jpg"
+  },
+  {
+    "revision": "7dcb95a3c8b771216da85a8dfd4ad666",
+    "url": "/travel-agency/static/media/img2.7dcb95a3.jpg"
+  },
+  {
+    "revision": "9da07cade246d45302eb59ada1cb0315",
+    "url": "/travel-agency/static/media/img3.9da07cad.jpg"
+  }
+]);
